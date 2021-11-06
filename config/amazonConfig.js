a037707b043549cfaa027363c9d64671a736d905
Arvin Poddar <arvin.poddar@gmail.com>
2021-11-06 17:22:09 -0400

    Configure AWS S3 Client for deployment

--- config/amazonConfig.js
+++ config/amazonConfig.js
@@ -1,8 +1,24 @@
-module.exports = {
-  awsConfig: {
-    ACCESS_ID: 'AKIA3PMOJFH3RFFMQVUP',
-    SECRET: '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',
-    BUCKET_NAME: 'plutus-apollo-images',
-    REGION: 'us-east-2'
-  }
+const AWS = require('aws-sdk');
+
+let configJson = null
+if (!process.env.deployed) {
+  configJson = require('./aws-plutus-config.json')
 }
+
+const AWSConfig = {
+  ACCESS_ID: process.env.deployed ? process.env.AWS_ACCESS_ID : configJson.ACCESS_ID,
+  SECRET: process.env.deployed ? process.env.AWS_SECRET_KEY : configJson.SECRET,
+  REGION: process.env.deployed ? process.env.AWS_REGION : configJson.REGION,
+  BUCKET_NAME: process.env.deployed ? process.env.AWS_BUCKET_NAME : configJson.BUCKET_NAME
+}
+
+S3Client = new AWS.S3({
+  accessKeyId: AWSConfig.ACCESS_ID,
+  secretAccessKey: AWSConfig.SECRET,
+  region: AWSConfig.REGION,
+  params: {
+    Bucket: AWSConfig.BUCKET_NAME,
+  }
+});
+
+module.exports = { S3Client, AWSConfig };
--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,6 +1,5 @@
-const { sendError, sendCustomError } = require('../utils');
-const AWS = require('aws-sdk');
-const { awsConfig } = require('../config/amazonConfig')
+const { sendError } = require('../utils');
+const { S3Client, AWSConfig } = require('../config/amazonConfig')
 
 const express = require('express');
 const router = express.Router();
@@ -15,15 +14,6 @@ router.post('/:userId/images/', async (req, res) => {
   const USER_ID = req.params.userId
 
   try {
-    var bucket = new AWS.S3({
-      accessKeyId: awsConfig.ACCESS_ID,
-      secretAccessKey: awsConfig.SECRET,
-      region: awsConfig.REGION,
-      params: {
-        Bucket: awsConfig.BUCKET_NAME,
-      }
-    });
-
     const suffix = uuidv4().substring(0, 6)
     const imageContent = Buffer.from(req.body.base64.replace(/^data:image\/\w+;base64,/, ""), 'base64')
     const imageName = `${USER_ID}-${suffix}.jpeg`
@@ -35,11 +25,11 @@ router.post('/:userId/images/', async (req, res) => {
       ContentType: 'image/jpeg'
     };
 
-    bucket.putObject(data, function (err, data) {
+    S3Client.putObject(data, function (err, data) {
       if (err) {
         sendError(res, err)
       } else {
-        const s3Url = `https://${awsConfig.BUCKET_NAME}.s3.${awsConfig.REGION}.amazonaws.com/${imageName}`
+        const s3Url = `https://${AWSConfig.BUCKET_NAME}.s3.${AWSConfig.REGION}.amazonaws.com/${imageName}`
         return res.send({
           url: s3Url
         })
