45015fc2624a9933396c30fc24781d24ef679861
Arvin Poddar <arvin.poddar@gmail.com>
2021-11-06 15:07:05 -0400

    Extend request payload size

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 
 PREFIX_ROUTE = "/users"
